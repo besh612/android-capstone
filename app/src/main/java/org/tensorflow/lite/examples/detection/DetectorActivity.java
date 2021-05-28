874b0337072d62419639cde6e74b2f2569c585e3
besh612 <sung.hun5016@gmail.com>
2021-05-28 11:17:48 +0900

    add new training model

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -83,7 +83,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   // Configuration values for the prepackaged SSD model.
   private static final int TF_OD_API_INPUT_SIZE = 300;
   private static final boolean TF_OD_API_IS_QUANTIZED = false;
-  private static final String TF_OD_API_MODEL_FILE = "crack.tflite";
+  private static final String TF_OD_API_MODEL_FILE = "model.tflite";
   private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/crack.txt";
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
   // Minimum detection confidence to track a detection.
